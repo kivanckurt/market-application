4a8cd1824ab742811cbd6277656e7f0e2359a402
Erdem Erdoğdu <erdemerdogdu70@gmail.com>
2024-05-18 21:07:02 +0300

    Update customer_edit_profile.php

--- 256/customer_edit_profile.php
+++ 256/customer_edit_profile.php
@@ -9,26 +9,66 @@
         header("location: customer_login.php");
         exit;
     }
-    var_dump($_SESSION);
-
+    //var_dump($_SESSION);
+    $error=[];
     //generate shopping cart if it is not set
     $user = &$_SESSION["customer_user"];
     $cart =$_SESSION["customer_user"]["cart"];
     if($_SERVER["REQUEST_METHOD"] == "POST"){
-      var_dump($_POST);
+      //var_dump($_POST);
       extract($_POST);
+      if(array_key_exists('email',$_POST)){
+      $name=htmlspecialchars($name);
+      $city=htmlspecialchars($city);
+      $district=htmlspecialchars($district);
+      $address=htmlspecialchars($address);
+      $email=htmlspecialchars($email);
+
+      if (filter_var($email, FILTER_VALIDATE_EMAIL)===false) {
+      $error["email"]="Email is in a incorrect format";
+      }
+      if (!$name) {
+          $error["name"]="Name cannot be empty";
+      }
+      if (!$city) {
+          $error["city"]="District cannot be empty";
+      }
+      if (!$district) {
+          $error["district"]="District cannot be empty";
+      }
+      if (!$address) {
+          $error["address"]="Address cannot be empty";
+      }
+      }
+
+
+
+      if(empty($error)){
       $customer_id = $user["customer_id"];
       if(!empty($_POST) && isset($customer_id) && isset($email) && isset($name) && isset($city) && isset($district) && isset($address)){
-        var_dump($_SERVER);
+        //var_dump($_SERVER);
         updateCustomerInfo($customer_id, $email, $name, $city, $district, $address);
         validateCustomerUser($email, $user["password"], $user);
         $_SESSION["customer_user"]=$user;
         $_SESSION["customer_user"]["cart"]=$cart;
       }
+      }
     }
 
   //change password
   if(!empty($_POST) && isset($_POST["password"]) && isset($_POST["password_new"])){
+
+    if(!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,20}$/', $password_new)) {
+      $error["password"]="Password does not satify requirements";
+      /*
+      at least one lowercase char
+      at least one uppercase char
+      at least one digit
+      password lenght must be between 8-20
+      */
+    }
+    if(empty($error)){
+    
     // echo "<h1>READS PASSWORDS</h1>";
     $password_old = $user["password"];
     if(verifyPassword($password, $password_old)){      
@@ -38,7 +78,9 @@
       header("location: logout.php");
     }else{
       $message="Password does not match with old password";
+      $error["password"]="Old password is not correct";
       header("location: ?message=$message");
+    }
     }
   }
 
@@ -62,23 +104,23 @@
   <table>
     <tr>
       <td>Name</td>
-      <td><input type="text" name="name" id="email" value="<?= htmlspecialchars($user['name']) ?>"></td>
+      <td><input type="text" name="name" id="email" value="<?= isset($name) ? $name : htmlspecialchars($user['name']) ?>"></td>
     </tr>
     <tr>
       <td>Email</td>
-      <td><input type="text" name="email" id="email" value="<?= htmlspecialchars($user['email']) ?>"></td>
+      <td><input type="text" name="email" id="email" value="<?=isset($email) ? $email : htmlspecialchars($user['email']) ?>"></td>
     </tr>
     <tr>
       <td>City</td>
-      <td><input type="text" name="city" id="city" value="<?= htmlspecialchars($user['city']) ?>"></td>
+      <td><input type="text" name="city" id="city" value="<?=isset($city) ? $city : htmlspecialchars($user['city']) ?>"></td>
     </tr>
     <tr>
       <td>District</td>
-      <td><input type="text" name="district" id="district" value="<?= htmlspecialchars($user['district']) ?>"></td>
+      <td><input type="text" name="district" id="district" value="<?=isset($district) ? $district : htmlspecialchars($user['district']) ?>"></td>
     </tr>
     <tr>
       <td>Address</td>
-      <td><input type="text" name="address" id="address" value="<?= htmlspecialchars($user['address']) ?>"></td>
+      <td><input type="text" name="address" id="address" value="<?=isset($address) ? $address : htmlspecialchars($user['address']) ?>"></td>
     </tr>
     <tr>
       <td></td>
@@ -106,6 +148,29 @@
             </form>
         </table>
     </div>
+
+    <?php if (isset($error)){ ?>
+        <div class="errort">
+            <?php if (isset($error["email"])){ ?>
+                <p><?=$error["email"]?></p>
+            <?php  }?>
+            <?php if (isset($error["name"])){ ?>
+                <p><?=$error["name"]?></p>
+            <?php  }?>
+            <?php if (isset($error["password"])){ ?>
+                <p><?=$error["password"]?></p>
+            <?php  }?>
+            <?php if (isset($error["city"])){ ?>
+                <p><?=$error["city"]?></p>
+            <?php  }?>
+            <?php if (isset($error["district"])){ ?>
+                <p><?=$error["district"]?></p>
+            <?php  }?>
+            <?php if (isset($error["address"])){ ?>
+                <p><?=$error["address"]?></p>
+            <?php  }?>
+        </div>
+    <?php  }?>
     </section>
 </body>
 </html>
